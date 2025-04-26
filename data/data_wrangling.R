e1272d408bb95896885de1e74d05de8ea5d47d67
matthewmurnane <mmurnane00@gmail.com>
2025-04-25 17:38:56 -0700

    Initial Commit

---
+++ data/data_wrangling.R
@@ -0,0 +1,25 @@
+library(readr)
+library(tidyverse)
+library(janitor)
+
+sleep <- read_csv("data/Sleep_health_and_lifestyle_dataset.csv") %>% 
+  clean_names()
+
+glimpse(sleep)
+
+sleep <- sleep %>%
+  separate(blood_pressure, into = c("systolic", "diastolic"), sep = "/", convert = TRUE)
+
+sleep$stress_level <- as.factor(sleep$stress_level)
+
+sleep <- sleep %>%
+  mutate(
+    systolic = as.numeric(systolic),
+    diastolic = as.numeric(diastolic),
+    MAP = (2 * diastolic + systolic) / 3
+  )
+
+sleep <- sleep %>% 
+  select(!c(person_id, systolic, diastolic))
+
+glimpse(sleep)
